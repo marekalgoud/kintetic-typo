6e0212958c566aad16ce876947e277e7be0edc75
Algoud Marek <marek.algoud@smile.fr>
2020-06-08 20:50:40 +0200

    init typo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useMemo, useEffect} from 'react';
+import { Canvas, useThree, useFrame, createPortal, useRender } from 'react-three-fiber'
+import { Text, OrbitControls, PerspectiveCamera } from "drei"
+import * as THREE from 'three'
+import './materials/textShaderMaterial'
+
 import './App.css';
 
-function App() {
+function Camera() {
+
+  const { viewport } = useThree()
+  const cam = useRef()
+
+  // let zoom = 20
+  // if (viewport.width < 520) {
+  //   zoom = viewport.width / 320 * 12
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PerspectiveCamera ref={cam} makeDefault position={[0, 0, 40]}  />
+  )
+}
+
+
+function MovingText({children, position, rotation}) {
+  const ref = useRef()
+  const cam = useRef()
+  const mat = useRef()
+
+  const [scene, target] = useMemo(() => {
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color('white')
+    const target = new THREE.WebGLRenderTarget(2048, 2048)
+    return [scene, target]
+  }, [])
+
+  const clock = new THREE.Clock();
+  let time
+  useFrame(state => {
+    state.gl.setRenderTarget(target)
+    state.gl.render(scene, cam.current)
+    state.gl.setRenderTarget(null)
+    time = clock.getElapsedTime() % 1
+    mat.current.uniforms.time.value = clock.getElapsedTime()
+  })
+
+  useEffect(() => {
+    console.log(mat)
+  })
+
+  return (
+    <>
+    <PerspectiveCamera ref={cam} position={[0, 0, 10]} />
+    {createPortal(
+        <Text
+          color="#000000"
+          fontSize={6}
+          textAlign="justify"
+          font="/fonts/bangers.woff"
+          anchorX="center"
+          anchorY="middle">
+          {children}
+        </Text>,
+        scene
+      )}
+    <mesh ref={ref} position={position} rotation={rotation}>
+      <torusKnotBufferGeometry attach="geometry" args={[9, 3, 768, 3, 4, 3]} />
+      <textShaderMaterial ref={mat} attach="material" texture={target.texture} time={time} color="#000000" map={target.texture} />
+      {/* <meshStandardMaterial attach="material" color={color} map={target.texture} /> */}
+    </mesh>
+    </>
+  )
+}
+
+function App() {
+  return (<Canvas pixelRatio={window.devicePixelRatio}>
+    <Camera />
+    <OrbitControls
+      minPolarAngle={Math.PI / 4}
+      maxPolarAngle={3 * Math.PI / 4}
+      minAzimuthAngle={-Math.PI / 4}
+      maxAzimuthAngle={Math.PI / 4}
+      minZoom={20}
+       />
+    <ambientLight intensity={1} />
+
+    <MovingText position={[0, 0, 0]}>
+      ENDLESS
+    </MovingText>
+    </Canvas>
   );
 }
 
---
+++ src/materials/textShaderMaterial.js
@@ -0,0 +1,72 @@
+import * as THREE from "three"
+import { extend } from "react-three-fiber"
+
+class TextShaderMaterial extends THREE.ShaderMaterial {
+  constructor() {
+    super({
+      uniforms: {
+        color: { value: new THREE.Color("white") },
+        texture: { type: "t", value: undefined },
+        time: { type: "f", value: 0.0 }
+      },
+      vertexShader: /*glsl*/ `
+        varying vec2 vUv;
+        varying vec3 vPos;
+
+        void main() {
+          vUv = uv;
+          vPos = position;
+
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+        }
+      `,
+
+      fragmentShader: /*glsl*/ `
+        varying vec2 vUv;
+        varying vec3 vPos;
+
+        uniform sampler2D texture;
+        uniform vec3 color;
+        uniform float time;
+
+        void main() {
+          float shadow = clamp(vPos.z / 5., 0., 1.);
+          
+          float vTime = time * 0.2;
+          vec2 repeat = vec2(14.0, 3.0);
+          vec2 uv = fract(vUv * repeat + vec2(-vTime, 0.));
+
+          vec3 vTexture = texture2D(texture, uv).rgb;
+          // vTexture *= color;
+          // vTexture *= vec3(uv.x, uv.y, 1.);
+
+          gl_FragColor = vec4((1. - vTexture) * shadow, 1.);
+        }
+      `
+    })
+  }
+
+  get color() {
+    return this.uniforms.color.value
+  }
+
+  get texture() {
+    return this.uniforms.texture.value
+  }
+
+  set texture(v) {
+    this.uniforms.texture.value = v
+  }
+
+  get time() {
+    console.log('uniform', this.uniforms.time.value)
+    return this.uniforms.time.value
+  }
+
+  set time(v) {
+    this.uniforms.time.value = v
+  }
+
+}
+
+extend({ TextShaderMaterial })
